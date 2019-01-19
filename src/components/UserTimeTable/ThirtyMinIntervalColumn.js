bbbffbf05f0c0d0dad47a2d77a2a9af73fb578ed
minhle <leminh.ams@gmail.com>
2019-01-19 14:44:32 -0600

    Create the column file for the front end

---
+++ src/components/UserTimeTable/ThirtyMinIntervalColumn.js
@@ -0,0 +1,103 @@
+// Minh & Allison
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+export default class ThirtyMinInterval extends Component {
+    render () {
+        const startTimes = Array.from(Array(48).keys())
+        const endTimes = Array.from(Array(48).keys())
+        const styles = theme => ({
+            root: {
+                flexGrow: 1,
+            },
+            paper: {
+                padding: theme.spacing.unit,
+                textAlign: 'center',
+                color: theme.palette.text.secondary,
+            },
+        });
+        function FormRow(props) {
+            // const { classes } = props;
+            return (
+                <React.Fragment>
+                    <Grid item xs={4}>
+                        <Paper >{indexToTime(2)} </Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper >item</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper >item</Paper>
+                    </Grid>
+                </React.Fragment>
+            );
+        }
+        // FormRow.propTypes = {
+        //     classes: PropTypes.object.isRequired,
+        // };
+        // function NestedGrid(props) {
+        //     const { classes } = props;
+        //
+        //     return (
+        //         <div className={classes.root}>
+        //             <Grid container spacing={8}>
+        //                 <Grid container item xs={12} spacing={24}>
+        //                     <FormRow classes={classes} />
+        //                 </Grid>
+        //                 <Grid container item xs={12} spacing={24}>
+        //                     <FormRow classes={classes} />
+        //                 </Grid>
+        //                 <Grid container item xs={12} spacing={24}>
+        //                     <FormRow classes={classes} />
+        //                 </Grid>
+        //             </Grid>
+        //         </div>
+        //     );
+        // }
+        // NestedGrid.propTypes = {
+        //     classes: PropTypes.object.isRequired,
+        // };
+        return (
+            startTimes.map((startTime, index,) => {
+                return (
+                    <div>
+                        <div style = {{backgroundColor: '#7fbfbf', display: "flex", border: "solid 1px"}}>
+                            {/*{indexToTime(endTimes[index])}*/}
+                            {/*{withStyles(styles)(NestedGrid)}*/}
+                            {/*{NestedGrid}*/}
+                            <div >
+                                <Grid container spacing={8}>
+                                    <Grid container item xs={12} >
+                                        {/*<FormRow> {indexToTime(endTimes[index])}  </FormRow>*/}
+                                       {indexToTime(endTimes[index])}
+                                       
+                                    </Grid>
+                                    {/*<Grid container item xs={12} spacing={24}>*/}
+                                    {/*<FormRow />*/}
+                                    {/*</Grid>*/}
+                                    {/*<Grid container item xs={12} spacing={24}>*/}
+                                    {/*<FormRow  />*/}
+                                    {/*</Grid>*/}
+                                </Grid>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
+}
+function indexToTime(index) {
+    var hour = Math.floor(index/2)
+    var minute = index%2 * 30
+    minute = minute == 0 ? "00" : "30"
+    var time = hour < 12 ? hour + ":" + minute + " AM" : hour - 12 + ":" + minute + " PM"
+    time = time == "0:00 AM" ? "12:00 AM" : time
+    time = time == "0:30 AM" ? "12:30 AM" : time
+    time = time == "0:00 PM" ? "12:00 PM" : time
+    time = time == "0:30 PM" ? "12:30 PM" : time
+    return time
+}
