2dea065e64f6d382800c7f121280ed806d43fd8f
Allison Wu <allisonwu@allisons-mbp.dyndns.rice.edu>
2019-04-13 14:04:13 -0500

    Fixed indexing for event timing

--- src/components/user/UserTimeTable/ColumnOfCalendar.js
+++ src/components/user/UserTimeTable/ColumnOfCalendar.js
@@ -17,10 +17,16 @@ export default class CalendarCol extends Component {
                 {startTimes.map((startTime, index,) => {
 
                     var [start, end] = timeToIndex(this.props.startTime, this.props.endTime);
+                    // start = start + 9; end = end + 9;
                     var startDayIndex =this.props.pickedStartDate.getDay();
                     var startDayOfWeek = daysOfWeek[startDayIndex];
 
-                    if (startTime >= start && startTime <= end) {
+                    //
+                    // console.log(start);
+                    //
+                    //
+                    // console.log(end);
+                    if (startTime >= start && startTime < end) {
                         if (startDayOfWeek === this.props.day) {
                             this.color = ["Monday", "Wednesday", "Friday", "Sunday"].includes(this.props.day) ?
                                 'rgba(62, 155, 97, 0.9)' :
@@ -49,6 +55,10 @@ export default class CalendarCol extends Component {
 
 }
 function indexToTime(index) {
+    index == 30 ? index = -18 : index;
+    index == 31 ? index = -17: index;
+    index == 32 ? index =-16 : index;
+    index == 33 ? index = -15 : index;
     var hour = Math.floor(index/2) + 9;
     var minute = index % 2 * 30;
     minute = minute == 0 ? "00" : "30";
@@ -66,10 +76,15 @@ function timeToIndex(startTimeStr, endTimeStr) {
     var startHour = startTimeInt[0] - 9;
     var startMin = startTimeInt[1];
     var endTimeInt = String(endTimeStr).split(':');
-    var endHour = endTimeInt[0];
+    var endHour = endTimeInt[0] - 9;
     var endMin = endTimeInt[1];
     var startIndex = (startHour * 2) + (startMin / 30);
     var endIndex = (endHour * 2) + (endMin / 30);
+    console.log(endIndex);
+    endIndex == -18 ? endIndex = 30 : endIndex;
+    endIndex == -17 ? endIndex = 31 : endIndex;
+    endIndex == -16 ? endIndex = 32 : endIndex;
+    endIndex == -15 ? endIndex = 33 : endIndex;
     return [startIndex, endIndex];
 
 }
--- src/components/user/UserTimeTable/ColumnOfCalendarTimeLabel.js
+++ src/components/user/UserTimeTable/ColumnOfCalendarTimeLabel.js
@@ -5,13 +5,14 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 export default class CalendarColTime extends Component {
     render() {
-        const startTimes = Array.from(Array(34).keys());
+        const startTimes = Array.from(Array(36).keys());
         return (
             <Grid container xs = {12} spacing={4} style={{marginTop: '-7px'}}>
                 {startTimes.map((index) => (
                         <Grid item xs={12} style={{ fontSize: 10,
                             width: 100, height: 15, textAlign: 'right', marginRight: 5}}>
                             {index % 2 ? '' : indexToTime(index)}
+                            {/*{index == 35 ? indexToTime(index) : ''}*/}
                         </Grid>
                 ))}
             </Grid>
@@ -27,9 +28,13 @@ function indexToTime(index) {
     var minute = index%2 * 30;
     minute = minute == 0 ? "00" : "30";
     var time = hour < 12 ? hour + ":" + minute + " AM" : hour - 12 + ":" + minute + " PM";
+    time = time == "12:00 PM" ? "12:00 AM" : time;
+    time = time == "12:30 PM" ? "12:00 AM" : time;
+
     time = time == "0:00 PM" ? "12:00 PM" : time;
     time = time == "0:30 PM" ? "12:30 PM" : time;
     time = time == "13:00 PM" ? "1:00 AM" : time;
     time = time == "13:30 PM" ? "1:30 AM" : time;
+    time = time == "14:00 PM" ? "2:00 AM" : time;
     return time
 }
